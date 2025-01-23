bccf3a4f13cf5a4805152fedf0802c1d72284a06
Shitian Zhang <2261700194@qq.com>
2025-01-23 11:24:16 +0000

    test

--- Optimiser/setup.py
+++ Optimiser/setup.py
@@ -1,10 +1,7 @@
-"""
-aaa
-"""
 from setuptools import setup, find_packages
 
 setup(
-    name = 'Wind',  # Replace with your package name
+    name = 'Optimiser',  # Replace with your package name
     version = '0.1',
     packages =find_packages(),  # Automatically includes all folders with __init__.py
     install_requires = []  # Add any dependencies required by your local modules
