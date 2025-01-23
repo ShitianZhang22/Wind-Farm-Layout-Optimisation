674736471d4f809d44a341f14d564137c204d136
Shitian Zhang <2261700194@qq.com>
2025-01-23 11:19:24 +0000

    debug

--- Wind/setup.py
+++ Wind/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'Site',  # Replace with your package name
+    name = 'Wind',  # Replace with your package name
     version = '0.1',
     packages =find_packages(),  # Automatically includes all folders with __init__.py
     install_requires = []  # Add any dependencies required by your local modules
