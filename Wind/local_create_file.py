adee7de65d888b899053f4099051f0039c332b32
Shitian Zhang <2261700194@qq.com>
2025-01-29 17:48:43 +0000

    local process on wind data

---
+++ Wind/local_create_file.py
@@ -0,0 +1,45 @@
+"""
+This file is for creating a netCDF4 file to process and store the annual digest to a local file.
+The next step is in local_write.py.
+"""
+
+import netCDF4
+import numpy as np
+
+def process_wind(path):
+    """
+    This function is for creating a file for writing annual digest.
+    `path`: the file path for storing the data.
+    """
+
+    summary = netCDF4.Dataset(path, 'a', format='NETCDF4')
+
+    # spatial resolution (resolution * 0.1 degree)
+    resolution = 10
+
+    # create dimensions
+    # lat = summary.createDimension('latitude', 1801)
+    # lon = summary.createDimension('longitude', 3600)
+    # dir = summary.createDimension('direction', 8)
+
+    summary.createDimension('latitude', 181)
+    summary.createDimension('longitude', 360)
+    summary.createDimension('direction', 8)
+
+    # create variables
+    s_lat = summary.createVariable('latitude', 'float32', ('latitude',), compression='zlib')
+    s_lon = summary.createVariable('longitude', 'float32', ('longitude',), compression='zlib')
+    s_dir = summary.createVariable('direction', 'int8', ('direction',), compression='zlib')
+    s_speed = summary.createVariable('speed', 'float32', ('latitude', 'longitude', 'direction',), compression='zlib', least_significant_digit=2)
+    s_freqency = summary.createVariable('frequency', 'float32', ('latitude', 'longitude', 'direction',), compression='zlib', least_significant_digit=2)
+
+    s_lat[:] = np.arange(-90, 90.1, 0.1 * resolution)
+    s_lon[:] = np.arange(0, 360, 0.1 * resolution)
+    s_dir[:] = np.arange(0, 8, 1)
+
+    summary.close()
+
+
+if __name__ == '__main__':
+    test_data = 'backup/summary-1d.nc'
+    process_wind(test_data)
